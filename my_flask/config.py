1acb87a92894327ae968ca51458a275fe4a7c6b1
wangyongjun <353398034@qq.com>
2018-06-18 19:43:24 +0800

    update requirements.txt

--- my_flask/config.py
+++ my_flask/config.py
@@ -15,8 +15,9 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/test?charset=utf8'
     # SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://autotestuat:Autotestuat@123@192.168.10.2:3306/autotest_result?"
     #                            "charset=utf8")
-    SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://yongjun.wang:1qaz@WSX@192.168.10.2:3306/mitest_platform?"
-                               "charset=utf8")
+    # SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://yongjun.wang:1qaz@WSX@192.168.10.2:3306/mitest_platform?"
+    #                            "charset=utf8")
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/mitest_platform?charset=utf8"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_TEARDOWN = True
 
